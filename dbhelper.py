e1c4b063cf3e4bc36f7882bf77fe8856d7eb0203
prakhar070 <bansalprakhar.2266@gmail.com>
2019-04-07 13:02:53 +0530

    add new crimes functionality

--- dbhelper.py
+++ dbhelper.py
@@ -1,27 +1,40 @@
 import pymysql
 import dbconfig
+import datetime
 
 class DBHelper:
 
 	def connect(self,database="crimemap"):
 		return pymysql.connect(host = 'localhost', user = dbconfig.db_user, passwd = dbconfig.db_password, db=database)
 
-	def get_all_inputs(self):
+	def get_all_crimes(self):
 		connection = self.connect()
 		try:
-			query = "SELECT description FROM crimes;"
+			query = "SELECT latitude, longitude, date, category, description FROM crimes;"
 			with connection.cursor() as cursor:
 				cursor.execute(query)
-			return cursor.fetchall()
+				named_crimes = []
+				for crime in cursor:
+					print("fuck")
+					named_crime = {
+						'latitude': crime[0],
+						'longitude': crime[1],
+						'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+						'category': crime[3],
+						'description':crime[4]
+					}
+					named_crimes.append(named_crime)
+					print (named_crimes)  
+				return named_crimes 
 		finally:
 			connection.close()
 
-	def add_input(self,data):
+	def add_crime(self, category, date, latitude, longitude, description):
 		connection = self.connect()
 		try:
-			query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
-			with connection.cursor() as cursor:
-				cursor.execute(query)
+			query = "INSERT INTO crimes(category, date, latitude, longitude, description) VALUES (%s, %s, %s, %s, %s);"
+			with connection.cursor() as cursor:                                                                                                                                                                             
+				cursor.execute(query, (category, date, latitude, longitude, description))
 				connection.commit()
 		finally:
 			connection.close()
@@ -35,4 +48,6 @@ class DBHelper:
 				connection.commit()
 		finally:
 			connection.close()
+
+
 			
